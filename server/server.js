680ef43608383f81ee813774024bb0a28909f04b
LeeWang <99003659+Leewang31@users.noreply.github.com>
2022-10-26 11:24:21 +0900

    setting

--- server/server.js
+++ server/server.js
@@ -10,32 +10,32 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({extended: true})) // for parsing application/x-www-form-urlencoded
 
 let id = 4
-const todoList = [{
+let todoList = [{
     id: 1,
     title: "new todolist open",
     date: '2022-10-22',
-    checked: true,
-},{
+}, {
     id: 2,
     title: "todolist 1",
     date: '2022-10-22',
-    checked: true,
-},{
+}, {
     id: 3,
     title: "todolist 2",
     date: '2022-10-23',
-    checked: true,
 }]
+
+
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
 app.get('/todo/', (req, res) => {
+    todoList.sort((a, b) => a.id - b.id)
     res.json(todoList)
 })
 app.get('/todo/:date', (req, res) => {
     let list = []
-    todoList.forEach(e=>{
-        if(e.date=== req.params.date){
+    todoList.forEach(e => {
+        if (e.date === req.params.date) {
             list.push(e)
         }
     })
@@ -43,17 +43,64 @@ app.get('/todo/:date', (req, res) => {
 })
 app.post('/todo/', (req, res) => {
     console.log('req.body', req.body); //데이터 전송 확인
-    const {title,date,checked} = req.body;
+    const {title, date} = req.body;
     todoList.push({
             id: id++,
             title,
-            checked,
             date,
         }
     )
     return res.send('success')
 })
+app.get('/todo/id/:id', (req, res) => {
+    let list = []
+    console.log(req.params.id)
+    todoList.forEach(e => {
+        if (e.id === Number(req.params.id)) {
+            list.push(e)
+        }
+    })
+    res.json(list)
+})
+app.delete('/todo/delete/:id', (req, res) => {
+    let count = 0;
+    let temp = 0;
+    todoList.forEach(e => {
+        if (e.id === Number(req.params.id)) {
+            temp = count
+        }
+        count++;
+    })
+    todoList[temp].id = todoList.length;
+    todoList.sort((a, b) => a.id - b.id);
+    todoList.pop()
+    res.json(todoList)
+})
 
+app.post('/todo/edit/:id/', (req, res) => {
+    let count = 0;
+    let temp = 0;
+    let id = '';
+    todoList.forEach(e => {
+        if (e.id === Number(req.params.id)) {
+            temp = count
+        }
+        count++;
+    })
+    id = todoList[temp].id;
+
+    todoList[temp].id = todoList.length;
+    todoList.pop()
+
+    const {title, date} = req.body;
+    todoList.push({
+        id,
+        title,
+        date,
+    })
+    todoList.sort((a, b) => a.id - b.id);
+    res.json(todoList)
+})
 
 app.listen(4000, () => {
     console.log("server start!")
